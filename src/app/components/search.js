3fa14eb23fda70911a854bcad9219fe927cc9bdb
suhaildhaliwal <124386900+suhaildhaliwal@users.noreply.github.com>
2023-06-09 21:41:09 +0530

    Add files via upload

---
+++ src/app/components/search.js
@@ -0,0 +1,15 @@
+import styles from './Card.module.css'
+
+const Search = props => (
+    <form className="search" onInput={event => props.onInput(event.target.value)}>
+      <input type="search" value={props.query} placeholder={props.placeholder} />
+    </form>
+  );
+  
+//   Search.propTypes = {
+//     query      : PropTypes.string.isRequired,
+//     onInput    : PropTypes.func.isRequired,
+//     placeholder: PropTypes.string
+//   };
+  
+export default Search;
